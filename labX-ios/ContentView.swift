a9ecde2bf69710ae7e97d05696dba54ddb512485
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2025-03-17 10:57:43 +0800

    Initial Commit

---
+++ labX-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  labX-ios
+//
+//  Created by Avyan Mehra on 17/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ labX-ios/labX_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  labX_iosApp.swift
+//  labX-ios
+//
+//  Created by Avyan Mehra on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct labX_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
