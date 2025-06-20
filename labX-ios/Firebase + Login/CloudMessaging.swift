294b32bc2c6c3f0e8330b7457a45089e0a2fde78
indigoX234 <parthasarathy_dhanush@s2023.ssts.edu.sg>
2025-06-20 21:42:18 +0800

    set up FCM basics, still need to integrate

---
+++ labX-ios/Firebase + Login/CloudMessaging.swift
@@ -0,0 +1,18 @@
+//
+//  CloudMessaging.swift
+//  labX-ios
+//
+//  Created by Dhanush  on 20/6/25.
+//
+
+import SwiftUI
+
+struct CloudMessaging: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudMessaging()
+}
