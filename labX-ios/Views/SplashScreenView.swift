78a18793aae5087d8ac1c81997d43a45c38b929b
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2025-08-27 16:16:29 +0800

    Changed version

--- labX-ios/Views/SplashScreenView.swift
+++ labX-ios/Views/SplashScreenView.swift
@@ -99,7 +99,7 @@ struct SplashScreenView: View {
                 
                 // Version and branding
                 VStack(spacing: 4) {
-                    Text("Version Beta 1.0.1")
+                    Text("Release 1.0.0")
                         .font(.system(size: 12, weight: .medium))
                         .foregroundColor(.white.opacity(0.6))
                         .opacity(versionOpacity)
